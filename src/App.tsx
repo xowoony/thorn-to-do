a27b405a589da2cbb1ee1840c10ffe47f7d9ab56
xowoony <trumanblack@hanmail.net>
2023-04-16 16:08:51 +0900

    추가되는 input console에 찍어보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -71,6 +72,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 function ToDoList() {
-    return null;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          placeholder="오늘 해야할 일을 입력하세요"
+        />
+        <button>추가</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
