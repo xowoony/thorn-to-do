b31d0d11afc36412e0ec0138a8e777ed197a17e3
xowoony <trumanblack@hanmail.net>
2023-04-19 21:50:23 +0900

    immutability 1

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,16 +1,18 @@
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "./atoms";
 
-
-
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
-  // newCategory가 IToDo의 category 항목이라는 것을 알려줌
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // currentTarget으로 이벤트를 발생시킨 버튼의 name을 잡는다.
     const {
       currentTarget: { name },
     } = event;
+
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name };
+      return oldToDos;
+    });
   };
 
   return (
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -18,6 +18,7 @@ function ToDoList() {
         {toDos.map((toDo) => (
           // li가 있던 자리 - ToDo.tsx로 감
           // {...toDo} 이렇게 써줌으로 인해 prop으로 text, category, 등등 필수적으로 써주어야 하는 prop들을 써줄 노고가 줄어듦
+          // ToDoList는 ToDo를 렌더링 하고 있고 이 ToDo는 모든 props를 받고 있다.
           <ToDo key={toDo.id}{...toDo}/>
         ))}
       </ul>
